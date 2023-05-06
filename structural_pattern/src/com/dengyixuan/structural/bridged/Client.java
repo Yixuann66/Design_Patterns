70a5dd9e1b54c5680276c3a5cc513764b5bd0708
Yi Xuan Deng <850671214@qq.com>
2023-05-06 16:24:16 +0800

    05/06 creator pattern

---
+++ structural_pattern/src/com/dengyixuan/structural/bridged/Client.java
@@ -0,0 +1,23 @@
+package com.dengyixuan.structural.bridged;/**
+ * @Author 85067
+ * @create 06/05/2023 16:21
+ */
+
+/**
+ * @author 85067
+ * @version 1.0
+ * @description: 桥接模式
+ * <p>
+ * 桥接模式（Bridge Pattern），也叫做桥梁模式，结构型设计模式的一种，这个模式相对来说有些难理解。桥接，顾名思义，就是用来连接两个部分，为被分离了的抽象部分和实现部分搭桥。
+ * <p>
+ * 官方一点的说法叫：将抽象部分与它的实现部分分离，使它们都可以独立变化。
+ * <p>
+ * 还有另外一种理解方式：一个类存在两个（或多个）独立变化的维度，我们通过组合的方式，让这两个（或多个）维度可以独立进行扩展。
+ * @date 06/05/2023 16:21
+ */
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+}
--- structural_pattern/src/com/dengyixuan/structural/decorator/Client.java
+++ structural_pattern/src/com/dengyixuan/structural/decorator/Client.java
@@ -12,11 +12,14 @@ package com.dengyixuan.structural.decorator;/**
 public class Client {
     public static void main(String[] args) {
         FriesRice food = new FriesRice();
-        System.out.println(food.getPrice() + food.getDesc());
+        System.out.println(food.cost() + food.getDesc());
 
         System.out.println("============");
 
         FastFood egg = new Egg(food);
-        System.out.println(egg.getPrice() + egg.getDesc());
+        System.out.println(egg.cost() + egg.getDesc());
+
+        FastFood egg2 = new Egg(egg);
+        System.out.println(egg2.cost() + egg2.getDesc());
     }
 }
