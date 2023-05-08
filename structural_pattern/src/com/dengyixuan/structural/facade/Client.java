480e60f3ada8535562346caf5089509f7f715753
Yi Xuan Deng <850671214@qq.com>
2023-05-08 12:12:59 +0800

    05/08 structural pattern

--- structural_pattern/src/com/dengyixuan/structural/facade/Client.java
+++ structural_pattern/src/com/dengyixuan/structural/facade/Client.java
@@ -6,7 +6,16 @@ package com.dengyixuan.structural.facade;/**
 /**
  * @author 85067
  * @version 1.0
- * @description: 通过SmartApplicationFacade来操作子系统
+ * @description: 通 过 SmartApplicationFacade 来 操 作 子 系 统 对客户端屏蔽了子系统组件 外观聚合了子系统的组件
+ * <p>
+ * 不符合开闭原则，修改很麻烦，修改子系统，可能要修改外观类
+ * <p>
+ * 实现例子：
+ * Tomcat接受浏览器发来的请求，封装成servletRequest
+ * 封装成servletRequest，子接口HttpServletRequest
+ * <p>
+ * 其实request是RequestFacade的类的对象，使用到外观模式
+ * RequestFacade聚合了Request类（HttpServletRequest的子实现类）的对象
  * @date 08/05/2023 11:56
  */
 public class Client {
